ced217644d475823bedc62e26bc288f6e388edd5
최수현 <sw92522@naver.com>
2023-04-20 09:54:49 +0900

    create posts.views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,109 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.order_by('-pk')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('posts:detail', post.pk)
+    else:
+        form = PostForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/create.html', context)
+
+
+def detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    comments = post.comment_set.all()
+    comment_form = CommentForm()
+    context = {
+        'post': post, 
+        'comments': comments,
+        'comment_form': comment_form,
+    }
+    return render(request, 'posts/detail.html', context)
+
+
+@login_required
+def delete(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if post.user == request.user:
+        post.delete()
+    return redirect('posts:index')
+
+
+@login_required
+def update(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if post.user == request.user:
+        if request.method == 'POST':
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+                return redirect('posts:detail', post.pk)
+        form = PostForm(instance=post)
+        context = {
+            'post': post,
+            'form': form,
+        }
+        return render(request, 'posts/update.html', context)
+    else:
+        return redirect('posts:detail', post.pk)
+
+
+@login_required
+def comment_create(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.user = request.user
+        comment.post = post
+        comment.save()
+    return redirect('posts:detail', post.pk)
+
+
+@login_required
+def comment_delete(request, post_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if comment.user == request.user:
+        comment.delete()
+    return redirect('posts:detail', post_pk)
+
+
+@login_required
+def like(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.user in post.like_users.all():
+        post.like_users.remove(request.user)
+    else:
+        post.like_users.add(request.user)
+    return redirect('posts:detail', post_pk)
+
+
+@login_required
+def comment_like(request, post_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.user in comment.like_users.all():
+        comment.like_users.remove(request.user)
+    else:
+        comment.like_users.add(request.user)
+    return redirect('posts:detail', post_pk)
